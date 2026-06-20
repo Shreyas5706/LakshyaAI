cef4fde4d02a2e587aacc9aef48c1dab252002a6
Diya <diyapandya1012@gmail.com>
2026-06-20 10:24:16 +0530

    Deleted unnecessary files

--- Client/Server/routes/chatbot.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/auth.middleware");
-const chatbotController = require("../controllers/chatbot.controller");
-
-// POST /api/chatbot - Handles message queries from the authenticated user
-router.post("/", authMiddleware, chatbotController.handleChat);
-
-module.exports = router;
--- Client/Server/utils/skillsHash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-module.exports = function generateSkillsHash(skills) {
-  return crypto
-    .createHash('sha256')
-    .update(JSON.stringify([...skills].sort()))
-    .digest('hex')
-}
